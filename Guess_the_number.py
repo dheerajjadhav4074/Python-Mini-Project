5e3afcaa0fa36acdab40b880247c7b3cb7f65f97
Dheeraj Jadhav <jadhavdheeraj129@gmail.com>
2025-11-17 10:38:09 +0530

    Add files via upload

---
+++ Guess_the_number.py
@@ -0,0 +1,47 @@
+# Guess the Number
+import random 
+target = random.randint(1, 100)
+
+while True:
+    userChoice = input("Guess the target (or) Quit: ")
+    if (userChoice == "Q"):
+        break
+
+    userChoice = int(userChoice)
+    if (userChoice == target):
+        print("You guessed it right!")
+        break
+    elif (userChoice < target):
+        print("Too low!. Take a bigger guess...")
+    else:
+        print("Too high!. Take a smaller guess..")
+
+print("--- GAME OVER ---")
+    
+
+              
+# Output:
+
+'''
+Guess the target (or) Quit: 50
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 40
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 35 
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 25
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 15
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 10
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 4
+Too low!. Take a bigger guess...
+Guess the target (or) Quit: 8
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 6
+Too high!. Take a smaller guess..
+Guess the target (or) Quit: 5
+You guessed it right!
+--- GAME OVER ---
+'''
---
+++ Random_password.py
@@ -0,0 +1,28 @@
+# Random Password
+
+import random
+import string
+
+pass_len = 12
+
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+password = " "
+for i in range(pass_len):
+    password += random.choice(charValues)
+
+
+print("Your random password is:", password)
+
+
+# Output:
+
+'''
+Your random password is:  ^Tg?!U#pk9x5
+Your random password is:  GJosVo88Nsx9
+Your random password is:  {T+w_[C"t!TR
+Your random password is:  &{#FG_p+4vQ%                        
+Your random password is:  s0\`@Q>RE`!6
+Your random password is:  }C|&Y{,GAH(D
+                                                 
+'''
